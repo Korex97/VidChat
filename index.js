43a2a5c9f0a1257eb19aca0a298254024cd278bd
korede <egbeolowoakorede97@gmail.com>
2022-01-25 04:19:16 +0100

    Video Chat Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const {v4: uuidv4} = require("uuid"); 
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+//set View
+app.set("view engine", "ejs")
+app.use("/peerjs", peerServer)
+//Allow Public File Access
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`)
+    console.log("Video Chat in Progresss");
+})
+app.get("/:room", (req, res) => {
+    res.status(200).render("rooms", {
+        roomId: req.params.room
+    })
+});
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId)
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName)
+        })
+    });
+})
+
+server.listen(3030);
